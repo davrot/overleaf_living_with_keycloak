77e061b46281002be7b8842d3beaaa41b10eadea
David Rotermund <davrot@uni-bremen.de>
2024-12-10 17:50:14 +0100

    Add files via upload

--- docker/compose/overleafregister/data/add_user.py
+++ docker/compose/overleafregister/data/add_user.py
@@ -1,61 +1,54 @@
-import requests
+import requests  # type: ignore
 import json
-from requests.auth import HTTPBasicAuth
-from urllib.parse import quote
+from requests.auth import HTTPBasicAuth  # type: ignore
+
 
 def add_keycloak_user(username):
 
-    with open("config.json",'r') as file:
+    with open("config.json", "r") as file:
         config = json.load(file)
 
     token_url = f"{config['keycloak_url']}/realms/master/protocol/openid-connect/token"
     token_data = {
         "grant_type": "password",
         "username": config["admin_username"],
-        "password": config["admin_password"]
+        "password": config["admin_password"],
     }
     users_url = f"{config['keycloak_url']}/admin/realms/master/users"
-    
+
     # Get token
     try:
         response = requests.post(
             token_url,
             data=token_data,
-            auth=HTTPBasicAuth(config["client_id"], config["client_secret"])
+            auth=HTTPBasicAuth(config["client_id"], config["client_secret"]),
         )
         response.raise_for_status()
 
-    except requests.exceptions.HTTPError as e:
+    except requests.exceptions.HTTPError:
         return False
-       
+
     access_token = response.json()["access_token"]
     headers = {
         "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
 
     # Check if user exists
-    params = {
-        "username": username,
-        "exact": "true"  
-    }
+    params = {"username": username, "exact": "true"}
 
     try:
-        response = requests.get(
-            users_url,
-            headers=headers,
-            params=params
-        )
+        response = requests.get(users_url, headers=headers, params=params)
         response.raise_for_status()
-        
+
         # Response is a list of users matching the criteria
         users = response.json()
-        
+
         # If we found any users with exact username match, the user exists
         if len(users) > 0:
             return False
-        
-    except requests.exceptions.HTTPError as e:
+
+    except requests.exceptions.HTTPError:
         return False
 
     # Make new user
@@ -71,14 +64,10 @@ def add_keycloak_user(username):
 
     try:
         # Create the user
-        response = requests.post(
-            users_url,
-            headers=headers,
-            data=json.dumps(new_user)
-        )
+        response = requests.post(users_url, headers=headers, data=json.dumps(new_user))
         response.raise_for_status()
 
-    except requests.exceptions.HTTPError as e:
+    except requests.exceptions.HTTPError:
         return False
 
-    return True 
+    return True
