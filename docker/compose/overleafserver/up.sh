dfa8863f977b0d3148b535b17492fe815948f7a7
David Rotermund <davrot@uni-bremen.de>
2024-12-11 18:05:31 +0100

    Add files via upload

--- docker/compose/overleafserver/up.sh
+++ docker/compose/overleafserver/up.sh
@@ -1,3 +1,7 @@
-python3 configure_features.py
+docker compose down
+cd /docker/features
+sh _tools/configure_features.sh
+sh _tools/generate_prep.sh
+cd /docker/compose/overleafserver
 docker compose up -d
 
