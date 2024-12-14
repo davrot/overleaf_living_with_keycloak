503fabdab3b381d8b131db072d85432bac6abc2e
David Rotermund <davrot@uni-bremen.de>
2024-12-14 18:03:15 +0100

    Add files via upload

--- docker/features/_tools/generate_prep.sh
+++ docker/features/_tools/generate_prep.sh
@@ -2,6 +2,7 @@
 docker_path="/docker/compose/overleafserver/data/"
 mkdir -p ${docker_path}
 rm ${docker_path}/prep.sh
-for file in $(ls -v _prep/*.sh); do
+for file in $(ls -v _prep/*.sh | grep -v "^_prep/END_default.sh"); do
     cat "${file}" >> ${docker_path}/prep.sh
 done
+cat _prep/END_default.sh >> ${docker_path}/prep.sh
